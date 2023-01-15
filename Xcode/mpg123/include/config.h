1855b52ea33a89089c4237b691c9a8b01d55c0b1
Denis Popov <aoh@list.ru>
2023-01-15 17:53:42 +0300

    1.31.1;

--- Xcode/mpg123/include/config.h
+++ Xcode/mpg123/include/config.h
@@ -367,7 +367,7 @@
 #define NET123 1
 
 /* Define if network support is enabled. */
-/* #undef NETWORK */
+#define NETWORK 1
 
 /* Define to disable 16 bit integer output. */
 /* #undef NO_16BIT */
@@ -437,7 +437,7 @@
 #define PACKAGE_NAME "mpg123"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "mpg123 1.30.2"
+#define PACKAGE_STRING "mpg123 1.31.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "mpg123"
@@ -446,7 +446,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.30.2"
+#define PACKAGE_VERSION "1.31.1"
 
 /* Define if portaudio v18 API is wanted. */
 /* #undef PORTAUDIO18 */
@@ -488,7 +488,7 @@
 /* #undef USE_YASM_FOR_AVX */
 
 /* Version number of package */
-#define VERSION "1.30.2"
+#define VERSION "1.31.1"
 
 /* Define to use Win32 named pipes */
 /* #undef WANT_WIN32_FIFO */
